4af75d03b66d6c333f86f42891272167c3b59dcd
TheYoungSensei <sacre.christopher@hotmail.com>
2017-05-17 23:26:17 +0200

    Version Finale Papayoo

--- global.h
+++ global.h
@@ -37,8 +37,8 @@
 #define MAX_PLAYER 3
 #define ERRNO errno
 #define TEMPS_SELECT 3
-#define NOMBRE_MANCHE 2
-#define NOMBRE_TOUR 1
+#define NOMBRE_MANCHE 3
+#define NOMBRE_TOUR 0
 #define INSCRIPTION_TIME 60
 
 #define SYS(call) ((call) == -1) ? perror(#call ": ERROR"), exit(1) : 0
